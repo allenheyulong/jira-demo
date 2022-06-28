e0b76270bbe4bb1353db42b127d23dd5ee6f8f58
allenheyulong <1225661792@qq.com>
2022-06-28 09:43:42 +0800

    feat: 修改配置，了解husky暂存校验

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,11 +9,19 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p><a className="App-link" href="https://reactjs.org"
-          target="_blank"  rel="noopener noreferrer"
-        >Learn React
-        </a> </header>
-    </div> );
+        </p>{" "}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Learn React{" "}
+        </a>{" "}
+      </header>
+    </div>
+  );
 }
 
 export default App;
