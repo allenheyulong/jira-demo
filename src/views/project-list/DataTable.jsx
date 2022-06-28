63054fba53622df9dba5ff4d31764b289842b350
allenheyulong <1225661792@qq.com>
2022-06-28 15:37:58 +0800

    feat: 添加工程列表

--- src/views/project-list/DataTable.jsx
+++ src/views/project-list/DataTable.jsx
@@ -1,21 +1,25 @@
-import React from "react"
+import React from "react";
 export const DataTable = ({ lists, users }) => {
-    return <table>
-        <thead>
-            <tr>
-                <th>名称</th>
-                <th>负责人</th>
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>名称</th>
+          <th>负责人</th>
+        </tr>
+      </thead>
+      <tbody>
+        {lists.map((list) => {
+          return (
+            <tr key={list.id}>
+              <td>{list.name}</td>
+              <td>
+                {users.find(({ id }) => id === list.personId)?.name || "未知"}
+              </td>
             </tr>
-        </thead>
-        <tbody>
-            {
-                lists.map((list) => {
-                    return <tr key={list.id}>
-                        <td>{list.name}</td>
-                        <td>{users.find(({ id }) => id === list.personId)?.name || '未知'}</td>
-                    </tr>
-                })
-            }
-        </tbody>
+          );
+        })}
+      </tbody>
     </table>
-}
+  );
+};
--- src/views/project-list/SearchQuery.jsx
+++ src/views/project-list/SearchQuery.jsx
@@ -1,23 +1,36 @@
-import React from "react"
+import React from "react";
 
 export const SearchQuery = ({ param, setParam, users }) => {
-    return (
-        <form>
-            <input type="text" value={param.name} onChange={e => setParam({
-                ...param,
-                name: e.target.value
-            })} />
-            <select value={param.personId} onChange={e => setParam({
-                ...param,
-                personId: e.target.value
-            })}>
-                <option value={''}>负责人</option>
-                {
-                    users.map((user) => {
-                        return <option key={user.id} value={user.id}>{user.name}</option>
-                    })
-                }
-            </select>
-        </form>
-    )
-}
+  return (
+    <form>
+      <input
+        type="text"
+        value={param.name}
+        onChange={(e) =>
+          setParam({
+            ...param,
+            name: e.target.value,
+          })
+        }
+      />
+      <select
+        value={param.personId}
+        onChange={(e) =>
+          setParam({
+            ...param,
+            personId: e.target.value,
+          })
+        }
+      >
+        <option value={""}>负责人</option>
+        {users.map((user) => {
+          return (
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
+          );
+        })}
+      </select>
+    </form>
+  );
+};
